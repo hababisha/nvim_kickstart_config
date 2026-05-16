adf516873a841f3ba7f705008947c26a5f20f29d
hababisha <haileamlakderbie1@gmail.com>
2026-05-16 11:03:23 +0300

    fix: double input fix alacritty by disabling progressive keyboard enhancement protocol

--- init.lua
+++ init.lua
@@ -242,6 +242,15 @@ vim.api.nvim_create_autocmd('TextYankPost', {
   callback = function() vim.hl.on_yank() end,
 })
 
+-- Disable progressive keyboard enhancement protocol to fix Alacritty double inputs
+vim.api.nvim_create_autocmd('VimEnter', {
+  callback = function() io.stdout:write '\027[>1u' end,
+})
+
+vim.api.nvim_create_autocmd('VimLeavePre', {
+  callback = function() io.stdout:write '\027[<1u' end,
+})
+
 -- [[ Install `lazy.nvim` plugin manager ]]
 --    See `:help lazy.nvim.txt` or https://github.com/folke/lazy.nvim for more info
 local lazypath = vim.fn.stdpath 'data' .. '/lazy/lazy.nvim'
